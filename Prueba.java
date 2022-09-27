bbf75ab423fe99f4307d261ec5250aa5e29b3959
IvanzereP 26 <ivanm@IvanzereP26.dominioivanzerep.es>
2022-09-27 09:52:52 +0200

    Agregada edad y año de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,8 @@ public class Prueba {
 
     public static void main(String args[]) {
 
-        System.out.println("Martin Perez");
+        System.out.println("Martin Perez.");
+        System.out.println("Tengo 20 años y nací en 2002.");
 
     }
 
