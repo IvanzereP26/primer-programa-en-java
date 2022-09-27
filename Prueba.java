d829b89412e0bd81522c76a5b5dc50ca54bde67e
IvanzereP 26 <ivanm@IvanzereP26.dominioivanzerep.es>
2022-09-27 11:05:37 +0200

    Eliminado apellidos y agregado cantane

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
     public static void main(String args[]) {
 
-        System.out.println("Martin Perez.");
         System.out.println("Tengo 20 años y nací en 2002.");
         System.out.println("Mi comida favorita es la pasta");
+        System.out.println("Mi cantante favorito es Quevedo");
 
     }
 
